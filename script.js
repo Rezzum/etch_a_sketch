5b80ccfded2977c17c5f5dbe8c8c0965ad024177
Jakub Latoch <172971794+Rezzum@users.noreply.github.com>
2024-12-09 13:08:28 +0100

    Finalize all functionalities incl reset and custom

--- script.js
+++ script.js
@@ -1,78 +1,65 @@
 const container = document.querySelector(".container");
+const customGridButton = document.createElement("button");
+const divVertical = document.querySelectorAll(".grid_block_vertical");
+const resetGridButton = document.createElement("button");
+
+customGridButton.type = "button";
+customGridButton.innerHTML = "New Grid";
+container.insertBefore(customGridButton, divVertical[0]);
+resetGridButton.type = "button";
+resetGridButton.innerHTML = "Reset Grid";
+container.insertBefore(resetGridButton, divVertical[0]);
 
 function makeGrid(size = 16) {
     for (let i = 0; i < size; i++) {
-    const divVertical = document.createElement("div");
-    divVertical.className = "grid_block_vertical";
-    divVertical.style.cssText = "display: flex; background-color: black; width: 960px"
+        const divVertical = document.createElement("div");
+        divVertical.className = "grid_block_vertical";
+        divVertical.style.cssText = "display: flex; background-color: black; width: 960px";
     
         for (let j = 0; j < size; j++) {
 
             const divHorizontal = document.createElement("div");
             divHorizontal.className = "grid_block_horizontal";
             divHorizontal.style.cssText = "flex: 1 1 0px; background-color: red; margin: 1px; aspect-ratio: 1";
-            
-            //Use if you want the divs to be numbered like a matrix
-            /*if (i < 10 && j < 10) {
-                divHorizontal.innerHTML = `Div 0${i}0${j}`;
-            } else if (i < 10 && j >= 10) {
-                divHorizontal.innerHTML = `Div 0${i}${j}`;
-            } else if (i >= 10 && j < 10) {
-                divHorizontal.innerHTML = `Div ${i}0${j}`;
-            } else {
-                divHorizontal.innerHTML = `Div ${i}${j}`;
-            }*/
-
             divVertical.appendChild(divHorizontal);
         }   
-        container.appendChild(divVertical);
+    container.appendChild(divVertical);
     }
 } 
 
-const divHorizontal = document.querySelectorAll(".grid_block_horizontal");
+makeGrid();
 
-divHorizontal.forEach((element) => {
-    element.addEventListener("mouseenter", () => {
-        element.addEventListener("mouseleave", () => {
-            element.style.backgroundColor = "blue";
-        })
-    })
-})
-
-const customGridButton = document.createElement("button");
-const divVertical = document.querySelectorAll(".grid_block_vertical");
-
-customGridButton.type = "button";
-customGridButton.innerHTML = "New Grid";
-container.insertBefore(customGridButton, divVertical[0]);
+container.addEventListener("mouseout", (event) => {
+    if (event.target.className === "grid_block_horizontal") {
+        event.target.style.backgroundColor = "blue";
+    }
+});
 
 customGridButton.addEventListener("click", () => {
     const userGridSize = parseInt(prompt("How big is the new grid supposed to be?"));
 
-    if (isNaN(userGridSize) || userGridSize <= 0 || !Number.isInteger(userGridSize)) {
-        alert("Please try again with a positive integer > 0");
+    if (isNaN(userGridSize) || userGridSize <= 0 || !Number.isInteger(userGridSize) || userGridSize > 100) {
+        alert("Please try again with a positive integer 0 < n <= 100");
     }
 
-    divVertical.forEach((element) => {
-        container.removeChild(element);
-    })
+    clearGrid();
 
     makeGrid(userGridSize);    
 })
 
-const resetGridButton = document.createElement("button");
-
-resetGridButton.type = "button";
-resetGridButton.innerHTML = "Reset Grid";
-container.insertBefore(resetGridButton, divVertical[0]);
-
 resetGridButton.addEventListener("click", () => {
-    divVertical.forEach((element) => {
-        container.removeChild(element);
-    })
-})
+    clearGrid();
+    makeGrid();
+});
 
-makeGrid();
+resetGrid();
+
+function clearGrid() {
+    while (container.lastChild != resetGridButton) {
+        container.removeChild(container.lastChild);
+    }
+}
+
 
 
 
